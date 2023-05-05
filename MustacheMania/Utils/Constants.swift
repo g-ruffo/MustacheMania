86f82cb4634b0d2f14b7942cb876926e57127126
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-05-04 17:58:35 -0700

    feat: Create constants file

---
+++ MustacheMania/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  K.swift
+//  MustacheMania
+//
+//  Created by Grayson Ruffo on 2023-05-04.
+//
+
+import Foundation
+
+struct K {
+    
+    struct Navigation {
+        static let recordingsNav = "RecordingNavigation"
+    }
+}
