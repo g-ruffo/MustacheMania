2da0d6d2f23f23f735d40015f12ce266901624ef
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-05-04 17:35:03 -0700

    Initial Commit

---
+++ MustacheMania/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MustacheMania
+//
+//  Created by Grayson Ruffo on 2023-05-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
