97f22ca38f7ac3be1d5a0e303ebe3ca21a7b6ba2
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-05-08 15:30:50 -0700

    feat: Create node when user taps on screen

--- MustacheMania/Controller/RecordVideoViewController.swift
+++ MustacheMania/Controller/RecordVideoViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import SceneKit
+import SpriteKit
 import ARKit
 import ARVideoKit
 
@@ -16,14 +16,15 @@ class RecordVideoViewController: UIViewController {
     @IBOutlet weak var durationViewContainer: UIView!
     @IBOutlet weak var durationImageView: UIImageView!
     @IBOutlet weak var durationLabel: UILabel!
-    @IBOutlet weak var switchCameraMenuButton: UIBarButtonItem!
     @IBOutlet weak var sceneView: ARSCNView!
+    
     private var captureSession: AVCaptureSession?
     private let videoOutput = AVCaptureMovieFileOutput()
     private let videoPreviewLayer = AVCaptureVideoPreviewLayer()
     private var captureDevice: AVCaptureDevice?
     private var isUsingFrontCamera = true
-    var recorder:RecordAR?
+    private var recorder:RecordAR?
+    private var currentMustache: SCNNode?
 
     private var coreDataService = CoreDataService()
 
@@ -31,11 +32,56 @@ class RecordVideoViewController: UIViewController {
         super.viewDidLoad()
         // Set CoreDataService delegate.
         coreDataService.delegate = self
-        
+        sceneView.delegate = self
+        recorder = RecordAR(ARSceneKit: sceneView)
+
         // Setup recording duration views
         durationViewContainer.layer.cornerRadius = durationViewContainer.frame.height / 2
         durationViewContainer.alpha = 0
+        addTapGestureToSceneView()
     }
+    
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didReceiveTapGesture(_:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    @objc func didReceiveTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: sceneView)
+        guard let hitTestResult = sceneView.hitTest(location, types: [.featurePoint, .estimatedHorizontalPlane]).first
+            else { return }
+        let anchor = ARAnchor(transform: hitTestResult.worldTransform)
+        sceneView.session.add(anchor: anchor)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        recorder?.prepare(configuration)
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        recorder?.rest()
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    func generateSphereNode() -> SCNNode {
+        let planeGeometry = SCNPlane(width: 0.1, height: 0.2)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "mustache3")
+        planeGeometry.materials = [material]
+
+        let node = SCNNode()
+        node.geometry = planeGeometry
+        return node
+    }
+
     
     func checkUserPermissions() {
         switch AVCaptureDevice.authorizationStatus(for: .video) {
@@ -85,83 +131,38 @@ class RecordVideoViewController: UIViewController {
 
     
     func setupCamera() {
-        // Create the capture session.
-        let captureSession = AVCaptureSession()
-        
-        // Find the default video device.
-        guard let videoDevice = AVCaptureDevice.default(for: .video) else { return }
-        
-        
-        captureDevice = videoDevice
 
-        do {
-            // Wrap the video device in a capture device input.
-            let videoInput = try AVCaptureDeviceInput(device: videoDevice)
-            // If the input can be added, add it to the session.
-            if captureSession.canAddInput(videoInput) {
-                captureSession.addInput(videoInput)
-            }
-            if captureSession.canAddOutput(videoOutput) {
-                captureSession.addOutput(videoOutput)
-            }
-            
-            captureSession.sessionPreset = .high
-//            // Set the preview layers session.
-//            videoPreviewLayer.session = captureSession
-//            videoPreviewLayer.videoGravity = .resizeAspectFill
-            // Start session.
-            captureSession.startRunning()
-            setupARView()
-            // Set session as global variable.
-            self.captureSession = captureSession
-            
-        } catch {
-            // Configuration failed. Handle error.
-            print("Error configuring camera = \(error.localizedDescription)")
-        }
     }
-    
-    func setupARView() {
-         let anchor = AnchorEntity(plane: .horizontal, minimumBounds:[0.2, 0.2])
-         arView.scene.addAnchor(anchor)
-        var mustaches: [Entity] = []
-        for _ in 1...4 {
-            let box = MeshResource.generateBox(width: 0.04, height: 0.002, depth: 0.04)
-            let metalMaterial = SimpleMaterial(color: .gray, isMetallic: true)
-            let model = ModelEntity(mesh: box, materials: [metalMaterial])
-            
-            model.generateCollisionShapes(recursive: true)
-            mustaches.append(model)
-        }
-        
-        for (index, mustache) in mustaches.enumerated() {
-            let x = Float(index % 2)
-            let z = Float(index / 2)
-            mustache.position = [x * 0.1, 0 , z * 0.1]
-            anchor.addChild(mustache)
-        }
-     }
     
     @IBAction func recordButtonPressed(_ sender: UIButton) {
-        if videoOutput.isRecording { videoOutput.stopRecording() }
-        else {
-            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        if recorder?.status == .recording {
+            recorder?.stop()
+            let url = getDocumentsDirectory().appendingPathComponent("\(Date()).mp4")
+  
+            self.recordButton.setImage(UIImage(systemName: "stop"), for: .normal)
+            self.durationViewContainer.alpha = 0
+        } else {
+            recorder?.record()
+            self.recordButton.setImage(UIImage(systemName: "record.circle"), for: .normal)
+            self.durationViewContainer.alpha = 1
 
-            let fileUrl = paths[0].appendingPathComponent("output.mov")
-
-            try? FileManager.default.removeItem(at: fileUrl)
-
-            videoOutput.startRecording(to: fileUrl, recordingDelegate: self as AVCaptureFileOutputRecordingDelegate)
         }
-        
-            let buttonImage = self.videoOutput.isRecording ? UIImage(systemName: "record.circle") : UIImage(systemName: "stop")
-            self.recordButton.setImage(buttonImage, for: .normal)
-            self.durationViewContainer.alpha = self.videoOutput.isRecording ? 0 : 1
-            self.switchCameraMenuButton.isEnabled = self.videoOutput.isRecording
     }
     
-    @IBAction func changeCameraButtonPressed(_ sender: UIBarButtonItem) {
-        print("Switch Camera Pressed")
+    func exportVideo(url: URL) {
+        recorder?.export(video: url) { exported , _ in
+            if exported {
+                
+            }
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
     }
     
 }
@@ -171,9 +172,14 @@ extension RecordVideoViewController: CoreDataServiceDelegate {
 
 }
 
-// MARK: - AVCaptureFileOutputRecordingDelegate
-extension RecordVideoViewController: AVCaptureFileOutputRecordingDelegate {
-    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
-        print("Finished Recording")
+// MARK: - ARSCNViewDelegate
+extension RecordVideoViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard !(anchor is ARPlaneAnchor) else { return }
+        currentMustache?.removeFromParentNode()
+        currentMustache = generateSphereNode()
+        DispatchQueue.main.async {
+            node.addChildNode(self.currentMustache!)
+        }
     }
 }
