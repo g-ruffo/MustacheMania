b83cc815c18ce515b3c9a2e3032c661d2127e796
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-05-08 20:00:46 -0700

    feat: Set video cell values and add notification observer

--- MustacheMania/Controller/VideoListViewController.swift
+++ MustacheMania/Controller/VideoListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class VideoListViewController: UIViewController {
 
@@ -26,8 +27,41 @@ class VideoListViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         // Register reusable cell with collection view.
         collectionView.register(VideoCell.nib(), forCellWithReuseIdentifier: K.Cell.videoCell)
+        coreDataService.loadVideosFromDatabase()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(videosUpdated), name: NSNotification.Name("Update"), object: nil)
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func videosUpdated() {
+        coreDataService.loadVideosFromDatabase()
+    }
+    
+    func fileInDocumentsDirectory(fileName: String) -> String? {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        if let url = documentsURL {
+            let fileURL = url.appendingPathComponent(fileName)
+            return fileURL.path
+        }
+            return nil
+        }
+    
+    func getThumbnailImage(forUrl url: URL) -> UIImage? {
+        let asset: AVAsset = AVAsset(url: url)
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+
+        do {
+            let thumbnailImage = try imageGenerator.copyCGImage(at: CMTimeMake(value: 1, timescale: 60), actualTime: nil)
+            return UIImage(cgImage: thumbnailImage)
+        } catch let error {
+            print(error)
+        }
+
+        return nil
+    }
 }
 
 // MARK: - UICollectionViewDelegate
@@ -43,6 +77,17 @@ extension VideoListViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.Cell.videoCell, for: indexPath) as! VideoCell
+        let video = savedVideos[indexPath.row]
+        cell.durationLabel.text = "\(video.duration ?? "")"
+        cell.tagLabel.text = video.tag
+        if let fileName = video.videoName {
+            let destinationPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            let videoPath = destinationPath?.appendingPathComponent(fileName)
+            if let path = videoPath {
+                cell.videoPreviewImage.image = getThumbnailImage(forUrl: path)
+                cell.videoPreviewImage.contentMode = .scaleAspectFill
+            }
+        }
         
         return cell
     }
@@ -53,8 +98,7 @@ extension VideoListViewController: UICollectionViewDataSource {
 // MARK: - UICollectionViewFlowLayout
 extension VideoListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = collectionView.bounds.width / 4
-        return CGSize(width: size, height: size)
+        return CGSize(width: 120, height: 120)
     }
 }
 
